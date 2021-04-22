5b518f7c2bfaaec32a49341330e12bcb695bfeb9
reshetnikd <dmitry.reshetnik@icloud.com>
2021-04-22 21:49:40 +0300

    Initial Commit

---
+++ ToDoList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoList
+//
+//  Created by Dmitry Reshetnik on 22.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
